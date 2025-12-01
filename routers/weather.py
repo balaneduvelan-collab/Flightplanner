951e99d24244f425936a5728bd750ad1fddae8cb
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 02:53:11 +0530

    Update weather.py

--- routers/weather.py
+++ routers/weather.py
@@ -1,1 +1,9 @@
 
+from fastapi import APIRouter
+from app.services.weather_service import get_weather 
+
+router = APIRouter(prefix="/weather", tags=["Weather"]) 
+
+@router.get("/{icao}")
+async def weather(icao: str):
+    return await get_weather(icao)
