e9986bf8655dcf16d10612c61bef52bfb6efa05a
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 02:47:02 +0530

    Add configuration settings using Pydantic

--- app/config.py
+++ app/config.py
@@ -1,1 +1,9 @@
+from pydantic import BaseSettings 
+
+class Settings(BaseSettings):
+    AVWX_API_KEY: str = ""
+    CHECKWX_API_KEY: str = ""
+    OPENMETEO_ENABLED: bool = True 
+
+settings = Settings()
 
