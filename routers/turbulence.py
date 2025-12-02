445c6b5b8e3dc9e9302cbe01c49b953d850c43ba
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 21:13:49 +0530

    Update turbulence.py

--- routers/turbulence.py
+++ routers/turbulence.py
@@ -1,1 +1,9 @@
 
+from fastapi import APIRouter
+from app.services.turbulence_service import get_turbulence 
+
+router = APIRouter(prefix="/turbulence", tags=["Turbulence"]) 
+
+@router.get("/")
+async def turbulence(lat: float, lon: float):
+    return get_turbulence(lat, lon)
