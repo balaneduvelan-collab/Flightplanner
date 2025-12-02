2c622f2c7689a29ab3485d519a6ddda5631504d5
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 21:20:25 +0530

    Update regulatory_service.py

--- services/regulatory_service.py
+++ services/regulatory_service.py
@@ -1,1 +1,8 @@
-
+def check_regulations(data):
+    # simple rule: no flight below FL110 in controlled airspace
+    violation = data.altitude < 11000
+    return {
+        "altitude": data.altitude,
+        "violation": violation,
+        "message": "Below minimum altitude!" if violation else "OK"
+    }
