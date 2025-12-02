56a8dd6f66db118decb9d0d44f4f6b220f4288e7
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 21:17:05 +0530

    Update weather_service.py

--- services/weather_service.py
+++ services/weather_service.py
@@ -1,1 +1,9 @@
+import httpx 
 
+async def get_weather(icao):
+    # Temporary mock until API keys added
+    return {
+        "icao": icao,
+        "metar": "MOCK METAR DATA",
+        "taf": "MOCK TAF DATA"
+    }
