d038a0422c2f40af6132810e3f140b1338ade771
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 21:22:05 +0530

    Update geo.py

--- utils/geo.py
+++ utils/geo.py
@@ -1,1 +1,10 @@
 
+from geopy.distance import geodesic 
+
+def great_circle(origin, dest):
+    # mock coordinates for now
+    return [
+        {"lat": 40.6, "lon": -73.7},
+        {"lat": 41.0, "lon": -80.1},
+        {"lat": 34.0, "lon": -118.4},
+    ]
