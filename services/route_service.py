8c3fd590653969f7d09dc39fc16ca85973cb1a1b
balaneduvelan-collab <balaneduvelan@gmail.com>
2025-12-02 21:16:18 +0530

    Update route_service.py

--- services/route_service.py
+++ services/route_service.py
@@ -1,1 +1,10 @@
+from app.utils.geo import great_circle 
+
+def compute_route(data):
+    points = great_circle(data.origin, data.destination)
+    return {
+        "origin": data.origin,
+        "destination": data.destination,
+        "points": points
+    }
 
